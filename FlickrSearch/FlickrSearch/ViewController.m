6fe7c6296c8cc0e70a4f4330aaa7ae362187a187
John Bogil <johncbogil@gmail.com>
2014-12-30 20:37:16 -0500

    Made successfull connection with Flicker API

--- FlickrSearch/FlickrSearch/ViewController.m
+++ FlickrSearch/FlickrSearch/ViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by John Bogil on 12/30/14.
 //  Copyright (c) 2014 John Bogil. All rights reserved.
-//
+//  96c5ad81d7705c871661d4bfc152adff
 
 #import "ViewController.h"
 
@@ -17,7 +17,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_cork.png"]];
+    
+    self.textField.delegate = self;
+    
+    self.searches = [@[] mutableCopy];
+    self.searchResults = [@{} mutableCopy];
+    self.flickr = [[Flickr alloc]init];
+
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -29,5 +40,29 @@
     // TODO
 }
 
+#pragma mark UITextFieldDelegate Methods
 
+
+- (BOOL) textFieldShouldReturn:(UITextField *)textField {
+    
+    
+    
+    // 1
+    [self.flickr searchFlickrForTerm:textField.text completionBlock:^(NSString *searchTerm, NSArray *results, NSError *error) {
+        if(results && [results count] > 0) {
+            // 2
+            if(![self.searches containsObject:searchTerm]) {
+                NSLog(@"Found %lu photos matching %@", (unsigned long)[results count],searchTerm);
+                [self.searches insertObject:searchTerm atIndex:0];
+                self.searchResults[searchTerm] = results; }
+            // 3
+            dispatch_async(dispatch_get_main_queue(), ^{
+                // Placeholder: reload collectionview data
+            });
+        } else { // 1
+            NSLog(@"Error searching Flickr: %@", error.localizedDescription);
+        } }];
+    [textField resignFirstResponder];
+    return YES; 
+}
 @end
